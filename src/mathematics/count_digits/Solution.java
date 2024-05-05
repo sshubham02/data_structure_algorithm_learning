a63310a7b2c1d0afe6e927cb4cce5051b19a0e88
Shubham Pahurkar <shubham.pahurkar@outlook.com>
2024-05-05 16:21:52 +0530

    initial commit

---
+++ src/mathematics/count_digits/Solution.java
@@ -0,0 +1,11 @@
+package mathematics.count_digits;
+
+import java.util.logging.Logger;
+
+public class Solution {
+    private static final Logger logger = Logger.getLogger(Solution.class.getName());
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
