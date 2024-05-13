e4e316b1be38a201a3f44986d88ad7ac14ece45b
Shubham Pahurkar <shubham.pahurkar@outlook.com>
2024-05-13 10:16:05 +0530

    Factorial of a number.

--- src/mathematics/factorial_number/Solution.java
+++ src/mathematics/factorial_number/Solution.java
@@ -2,13 +2,23 @@ package mathematics.factorial_number;
 
 public class Solution {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 4;
         System.out.println("Factorial of n is "+factorialN(n));
+        System.out.println("Factorial of n without recursion is "+factorial(n));
     }
+    //with recursive
     private static int factorialN(int n){
         if(n==0){
             return 1;
         }
         return n*factorialN(n-1);
+    }
+    //without recursive function
+    private static int factorial(int n){
+        int res=1;
+        for(int i=1;i<=n;i++){
+            res = res*i;
+        }
+        return res;
     }
 }
